de01cedbcbafc4d3e5d898b76eaeb3f69323f4a9
Bernio <jendabernard@gmail.com>
2026-05-05 18:33:57 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,12 +3,12 @@
 // 3) Zkopíruj sem konfiguraci webové aplikace z Firebase Console
 // 4) Tento soubor nahraj spolu s index.html, manifest.json a sw.js
 
-export const firebaseConfig = {
-  apiKey: "SEM_DOPLN_API_KEY",
-  authDomain: "SEM_DOPLN_PROJECT.firebaseapp.com",
-  databaseURL: "https://SEM_DOPLN_PROJECT-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "SEM_DOPLN_PROJECT",
-  storageBucket: "SEM_DOPLN_PROJECT.firebasestorage.app",
-  messagingSenderId: "SEM_DOPLN_SENDER_ID",
-  appId: "SEM_DOPLN_APP_ID"
+const firebaseConfig = {
+  apiKey: "AIzaSyD3_U6guVjZZQ9_Nt6mammPRFU1kDtjfBI",
+  authDomain: "doub-93ec9.firebaseapp.com",
+  databaseURL: "https://doub-93ec9-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "doub-93ec9",
+  storageBucket: "doub-93ec9.firebasestorage.app",
+  messagingSenderId: "572382583135",
+  appId: "1:572382583135:web:724e566bd5a2c11da9feea"
 };
